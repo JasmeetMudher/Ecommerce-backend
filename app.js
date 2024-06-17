3e24557d75eade589e529322c62ac7447628b6d5
Nathan-999 <nathantnganga@gmail.com>
2024-06-17 09:14:49 +0300

    update

--- app.js
+++ app.js
@@ -5,29 +5,30 @@ const logger = require('morgan');
 const cors = require('cors');
 const app = express();
 
-
-//Import routes
-
+// Import routes
 const productsRoute = require('./routes/products');
 const usersRoute = require('./routes/orders');
-//const {router} = require("express/lib/application");
 
-//Use routes
-app.use('/api/orders', usersRoute);
-app.use('/api/products', productsRoute);
-
+// Apply CORS middleware
 app.use(cors({
-    origin: "*",
-    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT'],
-    allowedHeaders: 'Content-Type, Authorisation, Origin, X-Requested-With, Accept'
+    origin: "*", // Allow all origins
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT'], // Allow these methods
+    allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'X-Requested-With', 'Accept'] // Allow these headers
 }));
 
+// Logger middleware
 app.use(logger('dev'));
+
+// Parsing middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+// Routes
+app.use('/api/orders', usersRoute);
+app.use('/api/products', productsRoute);
 
 module.exports = app;
